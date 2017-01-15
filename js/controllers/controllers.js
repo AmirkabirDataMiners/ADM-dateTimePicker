e9beb0510fcf4e12c2c32ef3380d8019d289bc9f
Mehrshad Teimouri <mete.30.10@gmail.com>
2017-01-15 19:29:43 +0330

    Update controllers.js

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,23 +1,13 @@
 var app = angular.module('app', [
-    'ngRoute',
     'appControllers',
     'hljs',
     'ADM-dateTimePicker'
 ]);
 
-app.config(['$routeProvider', 'ADMdtpProvider',
+app.config(['ADMdtpProvider',
 
-    function($routeProvider, ADMdtp) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/home/home.html',
-                controller: 'homeControllers'
-            })
-            .when('/home', {
-                templateUrl: 'views/home/home.html',
-                controller: 'homeControllers'
-            });
- 
+    function(ADMdtp) {
+
         ADMdtp.setOptions({});
         
     }
